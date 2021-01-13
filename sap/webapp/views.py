045cf2377a764b374f2653a891e1184d371b0f7b
José Luis Ferrete <josel.ferrete@s-dos.es>
2021-01-13 13:42:21 +0100

    Integrando el Modelo a la Vista - Patrón MVT

--- sap/webapp/views.py
+++ sap/webapp/views.py
@@ -2,9 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from personas.models import Persona
+
+
 def bienvenido(request):
-    mensajes = {'msg1':'Valor mensaje 1', 'msg2':'Valor mensaje 2'}
-    return render(request, 'bienvenido.html', mensajes)
+
+    no_personas_var = Persona.objects.count()
+
+    return render(request, 'bienvenido.html', {'no_personas':no_personas_var})
 
 def despedirse(request):
     return HttpResponse('Despedida desde Django')
