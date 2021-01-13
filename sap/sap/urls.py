efbbbcfe21d729d20eb7bc9ea2be0e96d213494e
José Luis Ferrete <josel.ferrete@s-dos.es>
2021-01-13 07:41:38 +0100

    Added some paths

--- sap/sap/urls.py
+++ sap/sap/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from webapp.views import bienvenido, despedirse
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path('bienvenido/', bienvenido)
+    path('', bienvenido),
+    path('despedida', despedirse)
+
 ]
+
--- sap/webapp/views.py
+++ sap/webapp/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def bienvenido(request):
+    return HttpResponse('Hola mundo desde Django')
+
+def despedirse(request):
+    return HttpResponse('Despedida desde Django')
