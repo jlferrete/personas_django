1f8aff98f247e270e5da24e2ea634b22786dcfcf
José Luis Ferrete <josel.ferrete@s-dos.es>
2021-01-13 13:12:14 +0100

    Templates en Django

--- sap/webapp/views.py
+++ sap/webapp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def bienvenido(request):
-    return HttpResponse('Hola mundo desde Django')
+    return render(request, 'bienvenido.html')
 
 def despedirse(request):
     return HttpResponse('Despedida desde Django')
